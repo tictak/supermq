c705bc1a63885a67914a590270fef6904b8a1423
yangfuyong <yangfuyong@supercxp.com>
2017-09-04 13:09:10 +0800

    pd fix

--- producer_test.go
+++ producer_test.go
@@ -17,16 +17,15 @@ func (i *IPRoute) Output(a int, b string) error {
 
 func TestMProduer(t *testing.T) {
 	mp := NewMProducer()
-	if err := mp.AddRx("127.0.0.1:4150"); err != nil {
+	if err := mp.AddRx("192.168.29.246:4150"); err != nil {
 		t.Fatal(err)
 	}
 	mp.SetLogger(&IPRoute{}, LogLevelDebug)
 	i := 0
-	for {
-		i++
-		i = i % 10
-		strI := strconv.Itoa(i)
-		mp.MultiPublish("dst2otlnk", [][]byte{
-			[]byte(`{"key":"` + strI + `","dst_ip":"127.0.0.` + strI + `","out_link":"HZ-CT"}`)})
-	}
+	i++
+	i = i % 10
+	strI := strconv.Itoa(i)
+	mp.MultiPublish("dst2otlnk", [][]byte{
+		[]byte(`{"key":"` + strI + `","dst_ip":"127.0.0.` + strI + `","out_link":"HZ-CT"}`)})
+	mp.Stop()
 }
